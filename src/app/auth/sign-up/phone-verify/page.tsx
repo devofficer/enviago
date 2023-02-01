614477513f8ea46638d05ea8dbfb4411b0931e87
stevejinprodev <steve.jin.md@gmail.com>
2023-02-01 10:43:47 -0800

    fix: remove eslint warning

--- src/app/auth/sign-up/phone-verify/page.tsx
+++ src/app/auth/sign-up/phone-verify/page.tsx
@@ -1,13 +1,15 @@
-import Button from "@/components/Button";
-import Icon from "@/components/Icon";
-import IconButton from "@/components/IconButton";
-import OutlinedButton from "@/components/OutlinedButton";
-import TextField from "@/components/TextField";
+import Button from '@/components/Button';
+import Icon from '@/components/Icon';
+import IconButton from '@/components/IconButton';
+import TextField from '@/components/TextField';
 
 export default function Phone() {
   return (
     <div className="relative flex flex-col items-center justify-center h-full xl:px-[10rem] px-10">
-      <IconButton className="xl:top-[4rem] xl:left-[4rem] top-[2rem] left-[2rem] absolute" icon="backward" />
+      <IconButton
+        className="xl:top-[4rem] xl:left-[4rem] top-[2rem] left-[2rem] absolute"
+        icon="backward"
+      />
       <Icon type="phone" className="mb-[1rem]" />
       <p className="font-manrope-bold text-[1.375rem] text-black mb-[.75rem] leading-[1.45]">
         Enter the code
@@ -24,9 +26,7 @@ export default function Phone() {
       <p className="font-manrope-semibold text-[.75rem] text-center text-gray-dark leading-[1.5rem] mb-[1.125rem] px-[1.375rem]">
         Didn’ t get a code? <span className="text-blue">Click to resend</span>
       </p>
-      <Button className="bg-purple text-white mb-[0.5rem]">
-        Verify
-      </Button>
+      <Button className="bg-purple text-white mb-[0.5rem]">Verify</Button>
     </div>
-  )
+  );
 }
