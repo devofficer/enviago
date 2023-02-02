20b420672bcd9b2192cdfc57d9f9e2ec34ddaeb2
stevejinprodev <steve.jin.md@gmail.com>
2023-02-02 09:42:00 -0800

    feat: implement textfield component label animation

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,27 @@
+import OutlinedButton from '@/components/OutlinedButton';
+import Divider from '@/components/Divider';
+import ForwardButton from '@/components/ForwardButton';
+import TextField from '@/components/TextField';
+import Icon from '@/components/Icon';
+
+export default function ResetPassword() {
+  return (
+    <div className="flex flex-col items-center justify-center xl:px-[10.25rem] px-10 w-full h-full">
+      <p className="font-manrope-bold text-[1.375rem] text-black mb-1">
+        Welcome&nbsp;👋
+      </p>
+      <p className="font-manrope-semibold text-gray-dark text-[0.875rem] mb-[1.125rem]">
+        Start with <span className="text-blue">Enviago.com</span> today!
+      </p>
+      <TextField placeholder="Email" className="mb-4" />
+      <TextField placeholder="Password" className="mb-4" />
+      <TextField placeholder="Confirm Password" className="mb-4" />
+      <ForwardButton className="mb-[1.875rem]">Continue</ForwardButton>
+      <Divider className="mb-[1.5rem]">or</Divider>
+      <OutlinedButton>
+        <Icon className="mr-[1.313rem]" type="google" />
+        Continue with Google
+      </OutlinedButton>
+    </div>
+  );
+}
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,18 +1,56 @@
-import cls from 'classnames';
+'use client';
+
+import clsx from 'classnames';
+import { useMemo, useState } from 'react';
 
 type TextFieldProps = {
   placeholder?: string;
   className?: string;
   value?: string;
+  onChange?: (value: string) => void;
 };
 
 export default function TextField(props: TextFieldProps) {
+  const id = useMemo(() => `textfield-${new Date().getTime().toString()}`, []);
+  const [focused, setFocused] = useState(false);
+  const [text, setText] = useState(props.value || '');
+  const shrink = useMemo(() => focused || text, [focused, text]);
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value);
+  };
+
   return (
-    <input
-      type="text"
-      value={props.value}
-      className={cls('textfield', props.className)}
-      placeholder={props.placeholder}
-    />
+    <div
+      className={clsx('w-full', 'relative', 'flex flex-col', props.className)}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+    >
+      <label
+        htmlFor={id}
+        className={clsx(
+          'absolute top-0 left-0',
+          'font-manrope-semibold text-[12px]',
+          'transition-all',
+          'origin-top-left translate-x-[18px]',
+          'pointer-events-none',
+          shrink
+            ? 'translate-y-[12px] text-purple'
+            : 'translate-y-[28px] text-gray-cool'
+        )}
+      >
+        {props.placeholder}
+      </label>
+      <input
+        type="text"
+        id={id}
+        value={text}
+        className={clsx(
+          'textfield',
+          shrink ? 'pt-[32px] pb-[16px]' : 'py-[24px]'
+        )}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
