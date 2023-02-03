2ca7d27ccb102e0fc9063c4b2a09e44decb4bb78
stevejinprodev <steve.jin.md@gmail.com>
2023-02-03 04:50:17 -0800

    fix: complete background of auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import Icon from '@/components/Icon';
+import Image from 'next/image';
 
 export default function AuthLayout({
   children,
@@ -8,19 +9,36 @@ export default function AuthLayout({
   return (
     <div className="w-screen bg-gradient-to-r from-[#2961ff_50%] to-[#ffffff_50%]">
       <div className="container flex h-screen overflow-hidden bg-gradient-to-r from-[#2961ff_55%] to-[#ffffff_45%]">
-        <div className="w-[55%] py-10 px-12 text-white xl:pl-[9.375rem] xl:pr-[6.875rem]">
-          <Icon className="mb-10" type="logo" />
-          <p className="font-manrope-extrabold text-[4.75rem] leading-[4.75rem] mb-8">
-            Fast, safe, social payments
-          </p>
-          <p className="font-manrope-bold text-[1.375rem]">
-            Pay. Get paid. Shop. Share. Join tens of millions of people on
-            Enviago.
-          </p>
+        <div className="w-[55%] flex flex-col justify-between py-[2.5rem] px-12 xl:pl-[9.375rem] xl:pr-[6.875rem] bg-[url(/images/auth/bg.svg)] bg-no-repeat bg-right-bottom">
+          <div className="text-white">
+            <Icon className="mb-[5.625rem]" type="logo" />
+            <p className="font-manrope-extrabold text-[4.75rem] leading-[4.75rem] mb-[3rem]">
+              Fast, safe, social payments
+            </p>
+            <p className="font-manrope-bold text-[1.375rem] mb-[2rem]">
+              Pay. Get paid. Shop. Share. Join tens of millions of people on
+              Enviago.
+            </p>
+          </div>
+
+          <div className="flex flex-col">
+            <Image
+              className="self-center"
+              src="/images/auth/group1.png"
+              width={254}
+              height={136}
+              alt="group"
+            />
+            <Image
+              className="-mt-[1rem]"
+              src="/images/auth/group2.png"
+              width={254}
+              height={136}
+              alt="group"
+            />
+          </div>
         </div>
-        <div className="w-[45%]">
-          {children}
-        </div>
+        <div className="w-[45%]">{children}</div>
       </div>
     </div>
   );
