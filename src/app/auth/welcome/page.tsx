6c0bd1d328308ecd39f75312911f1feec4a0502a
stevejinprodev <steve.jin.md@gmail.com>
2023-02-03 08:01:14 -0800

    fix: update icon button with variant and hover effect

--- src/app/auth/welcome/page.tsx
+++ src/app/auth/welcome/page.tsx
@@ -1,23 +1,43 @@
 import OutlinedButton from '@/components/OutlinedButton';
 import Divider from '@/components/Divider';
-import LinkButton from '@/components/LinkButton';
 import ContainedButton from '@/components/ContainedButton';
+import Image from 'next/image';
+import Icon from '@/components/Icon';
+import IconButton from '@/components/IconButton';
 
 export default function Welcome() {
   return (
-    <div className="relative flex flex-col items-center justify-center xl:px-[10.25rem] px-10 w-full h-full">
+    <div className="flex flex-col items-center justify-center xl:px-[10.25rem] px-10 pt-[315px] w-full h-full">
+      <div className="bg-blue w-full h-[315px] absolute top-0 flex flex-col pt-[73px] rounded-b-[1rem] bg-[url(/images/auth/circle.svg)] bg-no-repeat bg-[right_-32px_bottom_3.2px]">
+        <Icon type="logo" className="self-center justify-self-center" />
+        <Image
+          className="self-end"
+          src="/images/auth/group1.png"
+          width={254}
+          height={136}
+          alt="group"
+        />
+        <Image
+          className="-mt-[1rem]"
+          src="/images/auth/group2.png"
+          width={254}
+          height={136}
+          alt="group"
+        />
+      </div>
       <p className="font-manrope-bold text-[1.375rem] text-black mb-1">
         Welcome&nbsp;👋
       </p>
       <p className="font-manrope-semibold text-gray-dark text-[0.875rem] mb-[1.125rem]">
         Sign up or log in to get started!
       </p>
-      <ContainedButton className="mb-[1.875rem]">Sign up</ContainedButton>
-      <OutlinedButton className="mb-[1.5rem]">
-        Log in
-      </OutlinedButton>
-      <Divider className="mb-[1.5rem]">or</Divider>
-      <LinkButton path="/auth/reset-password">Reset my password</LinkButton>
+      <ContainedButton className="mb-[.625rem]">Sign up</ContainedButton>
+      <OutlinedButton className="mb-[1.5rem]">Log in</OutlinedButton>
+      <Divider className="mb-[1.375rem]">or</Divider>
+      <div className="flex">
+        <IconButton icon="google" className="mr-[16px]" variant="circle" />
+        <IconButton icon="phone" variant="circle" />
+      </div>
     </div>
   );
 }
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,16 +1,32 @@
 import clsx from 'classnames';
-import type { Icons } from "./Icon"
+import type { Icons } from './Icon';
 import Icon from './Icon';
 
 type IconButtonProps = {
-  icon: Icons,
+  icon: Icons;
   className?: string;
+  variant?: 'circle' | 'square';
 };
 
-export default function IconButton(props: IconButtonProps) {
+export default function IconButton({
+  icon,
+  className,
+  variant = 'square',
+}: IconButtonProps) {
   return (
-    <div className={clsx("flex items-center justify-center w-[2.5rem] h-[2.5rem] bg-blue-ice rounded-[11px]", props.className)}>
-      <Icon type={props.icon} />
+    <div
+      className={clsx(
+        'flex items-center justify-center hover:scale-[1.25] transition-all p-[12px] ',
+        className,
+        {
+          'bg-blue-ice rounded-[11px] w-[2.5rem] h-[2.5rem]':
+            variant === 'square',
+          'w-[42px] h-[42px] rounded-full border border-gray-trans':
+            variant === 'circle',
+        }
+      )}
+    >
+      <Icon type={icon} className="w-full h-full" />
     </div>
-  )
+  );
 }
