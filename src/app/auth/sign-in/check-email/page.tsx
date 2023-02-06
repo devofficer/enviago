dd6dba9de0023cd05ae6802ad408013f332ec1a5
stevejinprodev <steve.jin.md@gmail.com>
2023-02-06 08:29:52 -0800

    feat: implement check email mobile responsive

--- src/app/auth/sign-in/check-email/page.tsx
+++ src/app/auth/sign-in/check-email/page.tsx
@@ -4,20 +4,25 @@ import IconButton from '@/components/IconButton';
 
 export default function CheckEmail() {
   return (
-    <div className="relative flex flex-col items-center justify-center xl:px-[10.25rem] px-10 w-full h-full">
+    <div className="relative flex flex-col items-center lg:justify-center justify-between xl:px-[10.25rem] px-10 pt-[120px] pb-[42px] lg:py-0 w-full h-full">
       <IconButton
         className="xl:top-[4rem] xl:left-[4rem] top-[2rem] left-[2rem] absolute"
         icon="backward"
       />
-      <Icon type="envelope" className='mb-[1.125rem] '/>
-      <p className="font-manrope-bold text-[1.375rem] text-black mb-[0.5rem]">
-        Check your email
-      </p>
-      <p className="font-manrope-semibold text-gray-dark text-[.875rem] text-center leading-[1.125rem] mb-[1.125rem]">
-        Password reset link has just been sent to
-        <br />
-        <span className='text-blue'>leonardoolivera@gmail.com</span>
-      </p>
+      <span className="absolute top-[2.625rem] lg:hidden text-[.875rem] font-manrope-semibold">
+        Reset Password
+      </span>
+      <div className='w-full text-center'>
+        <Icon type="envelope" className='mb-[1.125rem] mx-auto ' />
+        <p className="font-manrope-bold text-[1.375rem] text-black mb-[0.5rem]">
+          Check your email
+        </p>
+        <p className="font-manrope-semibold text-gray-dark text-[.875rem] text-center leading-[1.125rem] mb-[1.125rem]">
+          Password reset link has just been sent to
+          <br />
+          <span className='text-blue'>leonardoolivera@gmail.com</span>
+        </p>
+      </div>
       <Button className='bg-purple text-white'>Log in</Button>
     </div>
   );
