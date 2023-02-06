ae28e9c534d319bcd38ed0ffff551974c71d0ae0
stevejinprodev <steve.jin.md@gmail.com>
2023-02-06 08:46:24 -0800

    feat: implement sign up mobile responsive

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="w-screen lg:bg-gradient-to-r from-[#2961ff_50%] to-[#ffffff_50%]">
-      <div className="container flex h-screen overflow-hidden lg:bg-gradient-to-r from-[#2961ff_55%] to-[#ffffff_45%]">
+      <div className="lg:container flex h-screen overflow-hidden lg:bg-gradient-to-r from-[#2961ff_55%] to-[#ffffff_45%]">
         <div className="w-[55%] hidden lg:flex flex-col justify-between py-[2.5rem] px-12 xl:pl-[9.375rem] xl:pr-[6.875rem] bg-[url(/images/auth/bg.svg)] bg-no-repeat bg-right-bottom">
           <div className="text-white">
             <Icon className="mb-[5.625rem]" type="logo" />
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -3,25 +3,40 @@ import Divider from '@/components/Divider';
 import ForwardButton from '@/components/ForwardButton';
 import TextField from '@/components/TextField';
 import Icon from '@/components/Icon';
+import IconButton from '@/components/IconButton';
+import LinkButton from '@/components/LinkButton';
 
 export default function SignUp() {
   return (
-    <div className="flex flex-col items-center justify-center xl:px-[10.25rem] px-10 w-full h-full">
-      <p className="font-manrope-bold text-[1.375rem] text-black mb-1">
-        Welcome&nbsp;👋
+    <div className="relative flex flex-col items-center lg:justify-center justify-between xl:px-[10.25rem] px-10 pt-[120px] pb-[42px] lg:py-0 w-full h-full">
+      <IconButton
+        className="xl:top-[4rem] xl:left-[4rem] top-[2rem] left-[2rem] absolute lg:hidden"
+        icon="backward"
+      />
+      <span className="absolute top-[42px] lg:hidden text-[.875rem] font-manrope-semibold">
+        Sign up
+      </span>
+      <div className="w-full text-center">
+        <p className="font-manrope-bold text-[1.375rem] text-black mb-1">
+          Welcome&nbsp;👋
+        </p>
+        <p className="font-manrope-semibold text-gray-dark text-[0.875rem] mb-[1.125rem]">
+          Start with <span className="text-blue">Enviago.com</span> today!
+        </p>
+        <TextField placeholder="Email" className="mb-4" />
+        <TextField placeholder="Password" className="mb-4" />
+        <TextField placeholder="Confirm Password" className="mb-4" />
+        <ForwardButton className="mb-[1.875rem]">Continue</ForwardButton>
+        <Divider className="mb-[1.5rem]">or</Divider>
+        <OutlinedButton variant='secondary' className="mb-[1.5rem]">
+          <Icon className="mr-[1.313rem]" type="google" />
+          Continue with Google
+        </OutlinedButton>
+      </div>
+      <p className="text-[.75rem] font-manrope text-navy leading-[1.5rem]">
+        Already have an account?&nbsp;
+        <LinkButton path="/auth/sign-in">Sign in</LinkButton>
       </p>
-      <p className="font-manrope-semibold text-gray-dark text-[0.875rem] mb-[1.125rem]">
-        Start with <span className="text-blue">Enviago.com</span> today!
-      </p>
-      <TextField placeholder="Email" className="mb-4" />
-      <TextField placeholder="Password" className="mb-4" />
-      <TextField placeholder="Confirm Password" className="mb-4" />
-      <ForwardButton className="mb-[1.875rem]">Continue</ForwardButton>
-      <Divider className="mb-[1.5rem]">or</Divider>
-      <OutlinedButton variant='secondary'>
-        <Icon className="mr-[1.313rem]" type="google" />
-        Continue with Google
-      </OutlinedButton>
     </div>
   );
 }
