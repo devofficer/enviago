8cabbb55c2901082f93affe651ef7ab0bda61183
stevejinprodev <steve.jin.md@gmail.com>
2023-02-09 07:22:12 -0800

    feat: implement transactions page

--- src/app/main/transactions/page.tsx
+++ src/app/main/transactions/page.tsx
@@ -4,6 +4,33 @@ import transactions from '@/mock/transactions';
 export default function Home() {
   return (
     <div>
+      <ul className="flex flex-wrap text-[14px] font-manrope-bold text-center text-gray mb-[31px]">
+        <li>
+          <a
+            href="#"
+            className="inline-block px-[42px] py-[8px] text-white bg-blue rounded-[39px] active"
+            aria-current="page"
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="inline-block px-[42px] py-[8px] rounded-[39px] hover:text-gray-dark hover:bg-gray-light"
+          >
+            Sent
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="inline-block px-[42px] py-[8px] rounded-[39px] hover:text-gray-dark hover:bg-gray-light"
+          >
+            Requested
+          </a>
+        </li>
+      </ul>
       {transactions.map((trans, idx) => (
         <TransactionCard key={idx} className="mb-[16px]" data={trans} />
       ))}
