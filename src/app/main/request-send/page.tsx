2b5a24da856ec86dc5b221a9ac573401b7cdb371
stevejinprodev <steve.jin.md@gmail.com>
2023-02-09 17:20:58 -0800

    feat: implement send or request money page

--- src/app/main/request-send/page.tsx
+++ src/app/main/request-send/page.tsx
@@ -1,5 +1,8 @@
 import Icon from '@/components/Icon';
 import TextField from '@/components/TextField';
+import AttachmentSvg from '@/assets/attachment.svg';
+import Image from 'next/image';
+import Button from '@/components/Button';
 
 export default function RequestOrSend() {
   return (
@@ -23,13 +26,82 @@ export default function RequestOrSend() {
           </a>
         </li>
       </ul>
-      <div className="bg-white px-[45px] pt-[34px] pb-[50px] rounded-[18px]">
+      <div className="bg-white px-[45px] pt-[34px] pb-[50px] rounded-[18px] mb-[28px]">
         <TextField
           placeholder="Name, @username, email, phone"
           size="small"
           startAdornment={<Icon type="pen" />}
+          className="mb-[21px]"
         />
+        <div className="flex items-center mb-[21px]">
+          <AttachmentSvg className="mr-[14px]" />
+          <div>
+            <div className="mb-[5px] text-[14px] font-manrope-medium text-black">
+              Share your <span className="text-blue">Enviago.com</span> link
+            </div>
+            <div className="text-gray text-[12px] leading-[16px]">
+              Text, post, or share your link to get paid
+            </div>
+          </div>
+        </div>
+        <h2 className="text-[18px] font-manrope-bold leading-[24px] text-black mb-[9px]">
+          Top people
+        </h2>
+        <div className="grid grid-cols-1 gap-[9px]">
+          <div className="flex items-center px-[16px] py-[19px] bg-gray-pale rounded-[18px]">
+            <Image
+              src="/images/avatars/female.png"
+              width={101}
+              height={100}
+              alt="avatar"
+              className="mr-[14px] w-[39px] h-[39px]"
+            />
+            <div>
+              <div className="text-[14px] leading-[20px] text-dark mb-[4px]">
+                Gibby Radki
+              </div>
+              <div className="text-[12px] leading-[16px] text-gray">
+                @gibbyradki
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center px-[16px] py-[19px] bg-gray-pale rounded-[18px]">
+            <Image
+              src="/images/avatars/female.png"
+              width={101}
+              height={100}
+              alt="avatar"
+              className="mr-[14px] w-[39px] h-[39px]"
+            />
+            <div>
+              <div className="text-[14px] leading-[20px] text-dark mb-[4px]">
+                Sofietje Boksem
+              </div>
+              <div className="text-[12px] leading-[16px] text-gray">
+                @gibbyradki
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center px-[16px] py-[19px] bg-gray-pale rounded-[18px]">
+            <Image
+              src="/images/avatars/female.png"
+              width={101}
+              height={100}
+              alt="avatar"
+              className="mr-[14px] w-[39px] h-[39px]"
+            />
+            <div>
+              <div className="text-[14px] leading-[20px] text-dark mb-[4px]">
+                Saga Lindén
+              </div>
+              <div className="text-[12px] leading-[16px] text-gray">
+                @gibbyradki
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <Button>Continue</Button>
     </div>
   );
 }
