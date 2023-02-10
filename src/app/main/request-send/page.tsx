4e441dd2e04419d14bc7bdb9ae89af80214bd982
stevejinprodev <steve.jin.md@gmail.com>
2023-02-09 16:43:56 -0800

    feat: add start & end adornments to textfield

--- src/app/main/request-send/page.tsx
+++ src/app/main/request-send/page.tsx
@@ -1,3 +1,35 @@
+import Icon from '@/components/Icon';
+import TextField from '@/components/TextField';
+
 export default function RequestOrSend() {
-  return <></>;
+  return (
+    <div>
+      <ul className="flex flex-wrap text-[14px] font-manrope-bold text-center text-gray mb-[28px]">
+        <li>
+          <a
+            href="#"
+            className="inline-block px-[42px] py-[8px] text-white bg-blue rounded-[39px] active"
+            aria-current="page"
+          >
+            Request
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="inline-block px-[42px] py-[8px] rounded-[39px] hover:text-gray-dark hover:bg-gray-light"
+          >
+            Send
+          </a>
+        </li>
+      </ul>
+      <div className="bg-white px-[45px] pt-[34px] pb-[50px] rounded-[18px]">
+        <TextField
+          placeholder="Name, @username, email, phone"
+          size="small"
+          endAdornment={<Icon type="pen" />}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,21 +1,37 @@
 'use client';
 
 import clsx from 'classnames';
-import { useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 type TextFieldProps = {
   placeholder?: string;
   className?: string;
   value?: string;
   password?: boolean;
+  variant?: 'filled' | 'outlined';
+  size?: 'small' | 'medium' | 'large';
+  startAdornment?: React.ReactNode;
+  endAdornment?: React.ReactNode;
   onChange?: (_value: string) => void;
 };
 
-export default function TextField(props: TextFieldProps) {
+export default function TextField({
+  value,
+  password,
+  placeholder,
+  startAdornment,
+  endAdornment,
+  size = 'medium',
+  variant = 'outlined',
+  className,
+}: TextFieldProps) {
   const id = useMemo(() => `textfield-${new Date().getTime().toString()}`, []);
   const [focused, setFocused] = useState(false);
-  const [text, setText] = useState(props.value || '');
-  const shrink = useMemo(() => focused || text, [focused, text]);
+  const [text, setText] = useState(value || '');
+  const shrink = useMemo(
+    () => (focused || text) && size !== 'small',
+    [focused, text, size]
+  );
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setText(e.currentTarget.value);
@@ -23,40 +39,72 @@ export default function TextField(props: TextFieldProps) {
 
   return (
     <div
-      className={clsx(
-        props.className,
-        'w-full',
-        'relative',
-        'flex flex-col',
-        'text-left'
-      )}
+      className={clsx(className, 'w-full', 'relative', 'flex', 'text-left', {
+        'justify-start': !!startAdornment,
+        'justify-end': !!endAdornment,
+      })}
       onFocus={() => setFocused(true)}
       onBlur={() => setFocused(false)}
     >
-      {props.placeholder && (
+      {(startAdornment || endAdornment) && (
+        <div
+          className={clsx('flex items-center mx-[16px] z-10', {
+            'h-[52px]': size === 'small',
+            'h-[72px]': size === 'medium',
+          })}
+        >
+          {startAdornment || endAdornment}
+        </div>
+      )}
+      {placeholder && (
         <label
           htmlFor={id}
           className={clsx(
             'absolute top-0 left-0',
             'font-manrope-semibold text-[12px]',
             'transition-all',
-            'origin-top-left translate-x-[18px]',
+            'origin-top-left',
             'pointer-events-none',
-            shrink
-              ? 'translate-y-[12px] text-purple'
-              : 'translate-y-[28px] text-gray-cool'
+            'z-10',
+            {
+              'translate-y-[12px] text-purple': shrink && size === 'medium',
+              'translate-y-[28px] text-gray-cool': !shrink && size === 'medium',
+              'translate-y-[18px] text-gray-cool opacity-100': size === 'small',
+              'opacity-0': (focused || text) && size === 'small',
+              'translate-x-[18px]': !startAdornment,
+              'translate-x-[52px]': !!startAdornment,
+            }
           )}
         >
-          {props.placeholder}
+          {placeholder}
         </label>
       )}
       <input
-        type={props.password ? 'password' : 'text'}
+        type={password ? 'password' : 'text'}
         id={id}
         value={text}
         className={clsx(
-          'textfield',
-          shrink && props.placeholder ? 'pt-[32px] pb-[16px]' : 'py-[24px]'
+          'bg-white',
+          'text-black',
+          'text-[.875rem]',
+          'font-manrope-semibold',
+          'focus:ring-purple',
+          'focus:border-purple',
+          'focus:outline-none',
+          'absolute z-0',
+          'w-full',
+          {
+            'pt-[32px] pb-[16px]': shrink && placeholder && size === 'medium',
+            'py-[24px]': !(shrink && placeholder) && size === 'medium',
+            'py-[15px]': size === 'small',
+            'border border-gray-trans': variant === 'outlined',
+            'rounded-[15px]': size === 'medium',
+            'rounded-[25px]': size === 'small',
+            'pl-[18px]': !startAdornment,
+            'pl-[52px]': !!startAdornment,
+            'pr-[18px]': !endAdornment,
+            'pr-[52px]': !!endAdornment,
+          }
         )}
         style={{ textAlign: 'inherit' }}
         onChange={handleChange}
