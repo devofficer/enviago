6d87a60370cb7bc68f0e6c9a856a76e42ec7ee24
stevejinprodev <steve.jin.md@gmail.com>
2023-02-14 12:57:27 -0800

    feat: implement payment sent dialog

--- src/app/main/request-send/amount/page.tsx
+++ src/app/main/request-send/amount/page.tsx
@@ -4,12 +4,17 @@ import Avatar from '@/components/Avatar';
 import Button from '@/components/Button';
 import IconButton from '@/components/IconButton';
 import TextField from '@/components/TextField';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import PaymentMethod from '../payment-method';
 import TabContext from '../tab-context';
+import Dialog from '@/components/Dialog';
+import ConfirmSvg from '@/assets/confirm.svg';
+import Image from 'next/image';
+import Divider from '@/components/Divider';
 
 export default function Amount() {
   const { tab } = useContext(TabContext);
+  const [openDialog, setOpenDialog] = useState(true);
 
   return (
     <>
@@ -38,7 +43,48 @@ export default function Amount() {
         <TextField placeholder="Message" value="For rent" />
       </div>
       {tab === 'Send' && <PaymentMethod />}
-      <Button>Continue</Button>
+      <Button onClick={() => setOpenDialog(true)}>Continue</Button>
+      <Dialog
+        open={openDialog}
+        className="px-[34px] py-[23px] w-[388px]"
+        onClose={() => setOpenDialog(false)}
+      >
+        <ConfirmSvg className="mx-auto mb-[15px]" />
+        <p className="font-manrope-bold text-[22px] text-center leading-[32px] text-black mb-[13px]">
+          You’ve sent payment request to{' '}
+          <span className="text-blue">Gibby Radki</span>
+        </p>
+        <div className="px-[20px]">
+          <div className="flex flex-col items-center justify-center mx-[12px] pt-[18px] pb-[16px] bg-gray bg-opacity-[6%] rounded-[15px] mb-[14px]">
+            <span className="text-blue font-manrope-bold text-[24px] leading-[32px] mb-4">
+              $2,600
+            </span>
+            <span className="text-gray text-[12px] leading-[16px]">
+              Chilean peso
+            </span>
+          </div>
+          <div className="text-gray-steel text-[.875rem] leading-[18px] mb-[12px]">
+            Requested from
+          </div>
+          <div className="flex items-center mb-[12px]">
+            <Image
+              src="/images/avatars/female.png"
+              width={101}
+              height={100}
+              alt="avatar"
+              className="rounded-full w-[48px] h-[48px] mr-[15px]"
+            />
+            <span className="text-[.875rem] text-dark leading-[20px]">
+              Gibby Radki
+            </span>
+          </div>
+          <Divider color="secondary" className="mb-[20px]" />
+          <div className="flex items-center justify-between text-[13px] leading-[18px] mb-[25px]">
+            <span className="text-black">Request Date:</span>
+            <span className="text-gray-dark">Jan 14, 2023</span>
+          </div>
+        </div>
+      </Dialog>
     </>
   );
 }
