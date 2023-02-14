3cb5fd52ce4c639aec1bd47fb726ef682189f9ae
stevejinprodev <steve.jin.md@gmail.com>
2023-02-14 13:38:11 -0800

    feat: implement payment overview dialog

--- src/app/main/request-send/amount/page.tsx
+++ src/app/main/request-send/amount/page.tsx
@@ -16,7 +16,7 @@ export default function Amount() {
   const { tab } = useContext(TabContext);
   const [openRequestDialog, setOpenRequestDialog] = useState(false);
   const [openProcessingDialog, setOpenProcessingDialog] = useState(false);
-  const [, setOpenSendDialog] = useState(false);
+  const [openSendDialog, setOpenSendDialog] = useState(false);
 
   return (
     <>
@@ -125,6 +125,46 @@ export default function Amount() {
           </div>
           <div className="text-gray-steel text-[.875rem] leading-[18px] mb-[12px]">
             Requested from
+          </div>
+          <div className="flex items-center mb-[12px]">
+            <Image
+              src="/images/avatars/female.png"
+              width={101}
+              height={100}
+              alt="avatar"
+              className="rounded-full w-[48px] h-[48px] mr-[15px]"
+            />
+            <span className="text-[.875rem] text-dark leading-[20px]">
+              Gibby Radki
+            </span>
+          </div>
+          <Divider color="secondary" className="mb-[20px]" />
+          <div className="flex items-center justify-between text-[13px] leading-[18px] mb-[25px]">
+            <span className="text-black">Request Date:</span>
+            <span className="text-gray-dark">Jan 14, 2023</span>
+          </div>
+        </div>
+      </Dialog>
+      <Dialog
+        open={openSendDialog}
+        className="px-[34px] py-[23px] w-[388px]"
+        onClose={() => setOpenSendDialog(false)}
+      >
+        <ConfirmSvg className="mx-auto mb-[15px]" />
+        <p className="font-manrope-bold text-[22px] text-center leading-[32px] text-black mb-[13px]">
+          You’ve successfully transferred money.
+        </p>
+        <div className="px-[20px]">
+          <div className="flex flex-col items-center justify-center mx-[12px] pt-[18px] pb-[16px] bg-gray bg-opacity-[6%] rounded-[15px] mb-[14px]">
+            <span className="text-blue font-manrope-bold text-[24px] leading-[32px] mb-4">
+              $2,600
+            </span>
+            <span className="text-gray text-[12px] leading-[16px]">
+              Chilean peso
+            </span>
+          </div>
+          <div className="text-gray-steel text-[.875rem] leading-[18px] mb-[12px]">
+            Sent to
           </div>
           <div className="flex items-center mb-[12px]">
             <Image
