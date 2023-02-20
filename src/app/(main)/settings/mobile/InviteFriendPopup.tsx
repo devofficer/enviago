5c4eb0831110a0216243342efcf3260cd3952df6
stevejinprodev <steve.jin.md@gmail.com>
2023-02-20 10:24:49 -0800

    fix: change style of settings invite friend popup

--- src/app/(main)/settings/mobile/InviteFriendPopup.tsx
+++ src/app/(main)/settings/mobile/InviteFriendPopup.tsx
@@ -13,32 +13,34 @@ export default function InviteFriendPopup({ open, onClose }: PopupProps) {
     <Popup
       open={open}
       onClose={onClose}
-      className="h-[100vh] bg-gray-pale p-[20px]"
+      className="h-[100vh] bg-gray-pale py-[20px]"
     >
-      <div className="flex items-center justify-between mb-[50px]">
-        <IconButton icon="backward" color="secondary" onClick={onClose} />
-        <span className="text-[14px]">Payment methods</span>
-        <LangSwitcher />
+      <div className="px-[20px]">
+        <div className="flex items-center justify-between mb-[50px]">
+          <IconButton icon="backward" color="secondary" onClick={onClose} />
+          <span className="text-[14px]">Payment methods</span>
+          <LangSwitcher />
+        </div>
+        <p className="px-[28px] text-center font-manrope-bold text-[22px] leading-[32px] mb-[21px]">
+          Invite your friends.
+          <br />
+          Earn up to $100!
+        </p>
+        <p className="px-[24px] text-center text-gray-dark text-[14px] leading-[18px] mb-[25px]">
+          Earn $10 for each friend who sends $4 from a linked account within 14
+          days of the invite.{' '}
+          <Link href="#" className="text-blue">
+            Terms apply.
+          </Link>
+        </p>
+        <Button className="mb-[25px]">Share link</Button>
+        <ProgressBar value={20} className="mb-[18px]" />
+        <div className="flex justify-between text-[12px] mb-[28px]">
+          <span>2 / 10 Referred</span>
+          <span>$20 Earned</span>
+        </div>
       </div>
-      <p className="px-[28px] text-center font-manrope-bold text-[22px] leading-[32px] mb-[21px]">
-        Invite your friends.
-        <br />
-        Earn up to $100!
-      </p>
-      <p className="px-[24px] text-center text-gray-dark text-[14px] leading-[18px] mb-[25px]">
-        Earn $10 for each friend who sends $4 from a linked account within 14
-        days of the invite.{' '}
-        <Link href="#" className="text-blue">
-          Terms apply.
-        </Link>
-      </p>
-      <Button className="mb-[25px]">Share link</Button>
-      <ProgressBar value={20} className="mb-[18px]" />
-      <div className="flex justify-between text-[12px] mb-[28px]">
-        <span>2 / 10 Referred</span>
-        <span>$20 Earned</span>
-      </div>
-      <div className="bg-white rounded-[18px] px-[26px] py-[32px]">
+      <div className="bg-white rounded-[30px] px-[26px] py-[32px]">
         <TextField
           placeholder="Name, @username, email, phone"
           size="small"
